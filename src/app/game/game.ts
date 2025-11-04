f1b63edbab893dbe94b9d5c28d1057f627d91774
raphael <raphael.zwick8@gmail.com>
2025-11-04 06:37:24 +0100

    add game logic

--- src/app/game/game.ts
+++ src/app/game/game.ts
@@ -1,12 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameData } from '../../models/gameData';
+
 
 @Component({
   selector: 'app-game',
+  
   imports: [CommonModule],
   templateUrl: './game.html',
   styleUrl: './game.scss',
 })
-export class Game {
+export class Game implements OnInit{
+    pickCardAnimation = false;
+    game!: GameData; //game! = Non-Null Assertion Operator 
 
+    ngOnInit(): void{
+      this.newGame();
+    }
+
+    newGame(){
+      this.game = new GameData();
+      console.log(this.game);
+
+    }
+
+    takeCard(){
+      this.pickCardAnimation = true;
+    }
 }
